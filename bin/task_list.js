9a8583157d6419e11b8661d134a218766c2e83c1
Rizky Aditya <rizkyadityanugroho@gmail.com>
2025-04-13 12:38:13 +0700

    add better error handler

--- bin/task_list.js
+++ bin/task_list.js
@@ -4,20 +4,36 @@ import { TaskStatus } from "../task_model.js";
 import fs from "fs";
 
 let availableStatus = Object.values(TaskStatus);
+const filePath = "task.json";
 
 export function readTasks(status = undefined) {
-  const data = fs.readFileSync("task.json");
-  const tasks = JSON.parse(data);
-  if (status) {
-    if (!availableStatus.includes(status)) {
-      console.error(
-        `Invalid status, available options are ${availableStatus.join(", ")}`
-      );
-      return;
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        console.error("No task file initialized yet. Add a task first");
+        process.exit(1);
+      } else if (err.code === "EACCES") {
+        console.error(`Permission denied for file ${filePath}`);
+        process.exit(1);
+      } else {
+        throw err;
+      }
+    } else {
+      const tasks = JSON.parse(data);
+      if (status) {
+        if (!availableStatus.includes(status)) {
+          console.error(
+            `Invalid status, available options are ${availableStatus.join(
+              ", "
+            )}`
+          );
+          return;
+        }
+        const dataByStatus = tasks.filter((obj) => obj.status == status);
+        console.log(dataByStatus);
+      } else {
+        console.log(tasks);
+      }
     }
-    const dataByStatus = tasks.filter((obj) => obj.status == status);
-    console.log(dataByStatus);
-  } else {
-    console.log(tasks);
-  }
-};
+  });
+}
